c777e0cb30733cf281663266925a7c0e86ad0a16
Abhinav <abhinav2003parvatham@gmail.com>
2022-09-08 11:29:30 +0530

    Add files via upload

---
+++ Demo.js
@@ -0,0 +1,14 @@
+console.log("Welcome to node js");
+var a =  10;
+var b = 20;
+var c = a + b;
+console.log("The value of a is :" + a + "The value of b is :" + b);
+console.log("Sum is" + c);
+
+console.log(`A value is ${a} and B value is ${b} Sum is $(a+b)`);
+
+if(c == 30){
+    console.log("Sum is 30");
+}else {
+    console.log("Else");
+}
---
+++ Http.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function(req, res){
+res.writeHead(200,{'Content-Type': 'text/html'});    
+res.end("Welcome to <br/> node js");
+}).listen(3000);
+console.log("Server is listening on http://127.0.0.1:3000");
